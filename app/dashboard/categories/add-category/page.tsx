be6ab0839397373cf5bc85340ec7f82d31b333d7
socrateNz <sssnzogning0@gmail.com>
2025-03-24 08:38:34 +0100

    test

--- app/dashboard/categories/add-category/page.tsx
+++ app/dashboard/categories/add-category/page.tsx
@@ -1,10 +1,13 @@
 import AddCategory from '@/components/Dashboard/Categories/AddCategory'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <AddCategory />
+      <AddCategory>
+        <Button>{"Ajouter une categorie"}</Button>
+      </AddCategory>
     </div>
   )
 }
