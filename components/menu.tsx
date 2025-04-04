22ebcb2d0f6557e3e9866b3ef2b9b99fe5965fdf
socrateNz <sssnzogning0@gmail.com>
2025-04-04 19:17:52 +0100

    Article

--- components/menu.tsx
+++ components/menu.tsx
@@ -14,7 +14,7 @@ export function MenuComp() {
 
     const axiosClient = axiosConfig();
 
-    const {isSuccess, data, isError, isLoading} = useQuery({
+    const { isSuccess, data, isError, isLoading } = useQuery({
         queryKey: ["categoryv"],
         queryFn: () => {
             return axiosClient.get<any, AxiosResponse<Category[]>>(
@@ -25,34 +25,34 @@ export function MenuComp() {
 
     const [selected, setSelected] = useState("");
 
-    
+
     const pathname = usePathname();
-    
+
     useEffect(() => {
         const segments = pathname.split("/");
-        
+
         if (segments.length > 2 && segments[1] === "user") {
             const userCategory = segments[2];
             setSelected(userCategory);
         }
     }, [pathname]);
-    
+
     const checkUserCategory = () => {
         return isSuccess ? data.data.flatMap(x => x.title).includes(selected) : false;
-    };    
+    };
 
     return (
         <section className="grid place-items-center border-y overflow-x-auto scrollbar-hide">
             <div className="inline-flex  gap-3">
-                    {isSuccess && data.data.length > 0 &&
-                            data.data.map((x, i) => {
-                                    return (
-                                        <Link className={`${decodeURIComponent(selected) === x.title && "bg-[#0128AE] text-white"} font-oswald h-10 w-fit shrink-0 px-3 flex items-center`} key={i} href={`/user/${x.title}`}>
-                                            <span className="font-medium text-[14px] uppercase">{x.title}</span>
-                                        </Link>
-                                    )
-                                })
-                            }
+                {isSuccess && data.data.length > 0 &&
+                    data.data.filter(x => x.parent === null).map((x, i) => {
+                        return (
+                            <Link className={`${decodeURIComponent(selected) === x.title && "bg-[#0128AE] text-white"} font-oswald h-10 w-fit shrink-0 px-3 flex items-center`} key={i} href={`/user/${x.title}`}>
+                                <span className="font-medium text-[14px] uppercase">{x.title}</span>
+                            </Link>
+                        )
+                    })
+                }
             </div>
         </section>
     )
