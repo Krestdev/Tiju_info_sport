f38a1d57ae8e0ebd476f4dacd88d976de2dca0eb
Hansjason97 <hansadiogo@gmail.com>
2025-04-08 16:35:39 +0100

    working on slug metadata

--- hooks/usePublishedData.ts
+++ hooks/usePublishedData.ts
@@ -1,7 +1,7 @@
-import axiosConfig from "@/api/api"
+import axiosConfig from "@/api/api";
+import { fetchCategory } from "@/lib/api";
 import { sortArticles } from "@/lib/utils";
-import { useQuery } from "@tanstack/react-query"
-import { AxiosResponse } from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 const axiosClient = axiosConfig();
 const today = new Date();
@@ -9,10 +9,10 @@ const today = new Date();
 export const usePublishedArticles = () =>{
     const {data, isSuccess, isError, isLoading} = useQuery({
         queryKey: ["categories"],
-        queryFn: ()=> axiosClient.get<any, AxiosResponse<Category[]>>(`/category`)
+        queryFn: ()=> fetchCategory()
     });
-    const categories:Category[] = isSuccess ? data.data : [];
-    const publishedArticles:Article[] = isSuccess ? sortArticles(data.data.filter(cat => cat.articles.length > 0).flatMap(cat => cat.articles).filter(x=>x.status==="published")) : [];
+    const categories:Category[] = isSuccess ? data : [];
+    const publishedArticles:Article[] = isSuccess ? sortArticles(data.filter(cat => cat.articles.length > 0).flatMap(cat => cat.articles).filter(x=>x.status==="published")) : [];
     const todayArticles:Article[] = publishedArticles.filter(article=>new Date(article.created_at).getDate()=== today.getDate());
     const headline:Article[]= publishedArticles.filter(x=>x.headline=== true);
 
