5a1c6fc0335474b389f3613f0cb437d5546ec232
socrateNz <sssnzogning0@gmail.com>
2025-04-10 08:04:12 +0100

    logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -11,14 +11,14 @@ interface LogoProps {
   textClass?:string;
 }
 
-function Logo({ showName = true, className, logoSize=10, textClass }: LogoProps) {
+function Logo({ showName = true, className, logoSize=40, textClass }: LogoProps) {
   const { settings } = useStore();
   return (
     <Link
       href={"/"}
       className={cn("flex flex-row items-center gap-4 text-primary-hover", className)}
     >
-      <img src={settings.logo} alt="Logo" className={`h-10 w-10`} />
+      <img src={settings.logo} alt="Logo" className={`size-[${logoSize}px]`} />
       {showName && (
         <span className={cn("uppercase font-semibold font-mono text-lg", textClass)}>
           {settings.compagnyName}
