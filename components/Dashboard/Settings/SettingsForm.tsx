880abd7232193a41c2804eabd416be5540a53378
socrateNz <sssnzogning0@gmail.com>
2025-04-12 15:15:55 +0100

    profil

--- components/Dashboard/Settings/SettingsForm.tsx
+++ components/Dashboard/Settings/SettingsForm.tsx
@@ -23,13 +23,16 @@ const formSchema = z
         logo: z.any(),
         description: z.string().optional(),
         phone: z.string().optional(),
-        whatsapp: z.string().optional(),
         facebook: z.string().optional(),
         instagram: z.string().optional(),
         x: z.string().optional(),
         email: z.string().optional(),
         address: z.string().optional(),
     });
+
+type SettingItem = {
+    data: Settings;
+};
 
 
 const SettingsForm = () => {
@@ -39,7 +42,7 @@ const SettingsForm = () => {
         Authorization: `Bearer ${token}`,
         "Accept": "*/*",
         "x-api-key": "abc123",
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'application/json'
     });
 
     const sections = useQuery({
@@ -54,14 +57,17 @@ const SettingsForm = () => {
     const getSetting = useQuery({
         queryKey: ["settings"],
         queryFn: () => {
-            return axiosClient.get<any, AxiosResponse<{ data: Settings }[]>>(
+            return axiosClient.get<any, AxiosResponse<Settings[]>>(
                 `/param/show
     `
             );
         },
     });
 
-    const setting: { data: Settings }[] = getSetting.isSuccess ? getSetting.data.data : [];
+
+
+    const setting: Settings[] = getSetting.isSuccess ? getSetting.data.data : [];
+
     const section: { title: string, id: number, content: Ressource[] }[] = sections.isSuccess ? sections.data.data : [];
 
     const { settings, editSettings } = useStore()
@@ -78,7 +84,6 @@ const SettingsForm = () => {
                     logo: data.logo,
                     description: data.description,
                     phone: data.phone,
-                    whatsapp: data.phone,
                     facebook: data.facebook,
                     instagram: data.instagram,
                     x: data.x,
@@ -95,13 +100,12 @@ const SettingsForm = () => {
     const updateSettings = useMutation({
         mutationKey: ["settings"],
         mutationFn: (data: Settings) => {
-            return axiosClient.patch("/param/update",
+            return axiosClient.patch(`/param/update/${1}`,
                 {
-                    compagnyName: data.company,
+                    company: data.company,
                     logo: data.logo,
                     description: data.description,
                     phone: data.phone,
-                    whatsapp: data.phone,
                     facebook: data.facebook,
                     instagram: data.instagram,
                     x: data.x,
@@ -116,41 +120,50 @@ const SettingsForm = () => {
         },
     })
 
-    console.log(setting);
-
     useEffect(() => {
-        if (setting[0]?.data) {
+        if (getSetting.isSuccess && setting.length > 0) {
+            const firstData = setting[0];
+            console.log("firstData", firstData);
+
             form.reset({
-                company: setting[0]?.data?.company ?? "",
-                logo: setting[0]?.data?.logo ?? "",
-                description: setting[0]?.data?.description ?? "",
-                phone: setting[0]?.data?.phone ?? "",
-                whatsapp: setting[0]?.data?.whatsapp ?? "",
-                facebook: setting[0]?.data?.facebook ?? "",
-                instagram: setting[0]?.data?.instagram ?? "",
-                x: setting[0]?.data?.x ?? "",
-                email: setting[0]?.data?.email ?? "",
-                address: setting[0]?.data?.address ?? "",
+                company: firstData.company ?? settings.compagnyName,
+                logo: firstData.logo ?? settings.logo,
+                description: firstData.description ?? settings.description,
+                phone: firstData.phone ?? settings.phone,
+                facebook: firstData.facebook ?? settings.facebook,
+                instagram: firstData.instagram ?? settings.instagram,
+                x: firstData.x ?? settings.x,
+                email: firstData.email ?? settings.email,
+                address: firstData.address ?? settings.address,
             });
         }
-    }, [getSetting.isSuccess])
+    }, [getSetting.isSuccess, getSetting.data, setting]);
 
+
+
+
+
+    const firstData = setting[0] ? setting[0] : null;
+
+    // console.log("setting[0]", setting[0]);
+    // console.log("setting[0]", setting[0].data);
+    // console.log("firstData", firstData);
 
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            company: setting[0]?.data?.company ?? settings.compagnyName,
-            logo: setting[0]?.data?.logo ?? settings.logo,
-            description: setting[0]?.data?.description ?? settings.description,
-            phone: setting[0]?.data?.phone ?? settings.phone,
-            whatsapp: setting[0]?.data?.whatsapp ?? settings.whatsapp,
-            facebook: setting[0]?.data?.facebook ?? settings.facebook,
-            instagram: setting[0]?.data?.instagram ?? settings.instagram,
-            x: setting[0]?.data?.x ?? settings.x,
-            email: setting[0]?.data?.email ?? settings.email,
-            address: setting[0]?.data?.address ?? settings.address,
+            company: firstData?.company ?? settings.compagnyName,
+            logo: firstData?.logo ?? settings.logo,
+            description: firstData?.description ?? settings.description,
+            phone: firstData?.phone ?? settings.phone,
+            facebook: firstData?.facebook ?? settings.facebook,
+            instagram: firstData?.instagram ?? settings.instagram,
+            x: firstData?.x ?? settings.x,
+            email: firstData?.email ?? settings.email,
+            address: firstData?.address ?? settings.address,
         }
-    })
+    });
+
 
 
 
@@ -183,33 +196,38 @@ const SettingsForm = () => {
                                 </FormItem>
                             )}
                         />
-                        <FormField
-                            control={form.control}
-                            name="logo"
-                            render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>{"Logo du site"}</FormLabel>
-                                    <FormControl>
-                                        <Input
-                                            type="file"
-                                            className="max-w-[384px] w-full"
-                                            onChange={(e) => {
-                                                const file = e.target.files?.[0];
-                                                if (file) {
-                                                    const reader = new FileReader();
-                                                    reader.onload = () => {
-                                                        setPhoto(reader.result as string);
-                                                        field.onChange(reader.result);
-                                                    };
-                                                    reader.readAsDataURL(file);
-                                                }
-                                            }}
-                                        />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )}
-                        />
+                        <div className='flex items-center gap-2'>
+                            {
+                                photo && <img src={photo} alt="" className='w-[300px] h-[300px] object-cover' />
+                            }
+                            <FormField
+                                control={form.control}
+                                name="logo"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>{"Logo du site"}</FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                type="file"
+                                                className="max-w-[384px] w-full"
+                                                onChange={(e) => {
+                                                    const file = e.target.files?.[0];
+                                                    if (file) {
+                                                        const reader = new FileReader();
+                                                        reader.onload = () => {
+                                                            setPhoto(reader.result as string);
+                                                            field.onChange(reader.result);
+                                                        };
+                                                        reader.readAsDataURL(file);
+                                                    }
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
                         <FormField
                             control={form.control}
                             name='description'
@@ -231,7 +249,7 @@ const SettingsForm = () => {
                             name='phone'
                             render={({ field }) => (
                                 <FormItem>
-                                    <FormLabel>{"Description du site"}</FormLabel>
+                                    <FormLabel>{"Telephone"}</FormLabel>
                                     <FormControl>
                                         <div className="relative max-w-[384px]">
                                             <span className="absolute flex items-center text-[16px] px-2 gap-2 top-1/2 transform -translate-y-1/2 h-full bg-[#0128AE] text-white">
@@ -250,7 +268,7 @@ const SettingsForm = () => {
                                 </FormItem>
                             )}
                         />
-                        <FormField
+                        {/* <FormField
                             control={form.control}
                             name='whatsapp'
                             render={({ field }) => (
@@ -272,7 +290,7 @@ const SettingsForm = () => {
                                     <FormMessage />
                                 </FormItem>
                             )}
-                        />
+                        /> */}
                         <FormField
                             control={form.control}
                             name='email'
