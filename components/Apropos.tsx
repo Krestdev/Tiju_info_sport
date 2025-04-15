2b20234c9469794fded207ae41b44ebf228f4073
socrateNz <sssnzogning0@gmail.com>
2025-04-15 18:10:42 +0100

    image in text

--- components/Apropos.tsx
+++ components/Apropos.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const Apropos = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+interface Props {
+    ressource: Ressource
+}
+
+
+const Apropos = ({ ressource }: Props) => {
+    return (
+        <div className='flex flex-col gap-4'>
+            h
+
+        </div>
+    )
 }
 
 export default Apropos
