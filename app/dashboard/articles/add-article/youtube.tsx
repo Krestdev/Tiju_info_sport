e956af1ba48c8fa4945b7d5969a21f7974457c33
Hansjason97 <hansadiogo@gmail.com>
2025-04-28 15:19:17 +0100

    fix on button youtube add

--- app/dashboard/articles/add-article/youtube.tsx
+++ app/dashboard/articles/add-article/youtube.tsx
@@ -35,7 +35,7 @@ function AddYoutubeVideo({addVideo}:Props) {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
         <DialogTrigger asChild>
-            <Button size={"icon"} variant={"ghost"}><FaYoutube/></Button>
+            <Button size={"icon"} variant={"ghost"} onClick={(e)=>{e.preventDefault(); setOpen(true)}}><FaYoutube/></Button>
         </DialogTrigger>
         <DialogContent>
             <DialogHeader>
