e3d87cf4a1f173514ba76a5291823987e95b51e4
Hansjason97 <hansadiogo@gmail.com>
2026-06-04 15:01:02 +0100

    feat: initialize root layout with dynamic metadata, global providers, and analytics integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,30 +12,52 @@ import GA from "@/components/GoogleAnalytics";
 import { fetchSettings } from "@/lib/api";
 
 const fontMono = Oswald({ subsets: ["latin"], variable: "--font-mono" });
-const fontSans = Open_Sans({ subsets: ["latin"], variable: "--font-sans" }); /**Ubuntu doesnt display well for some reason, so we better use this one :/ */
+const fontSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+}); /**Ubuntu doesnt display well for some reason, so we better use this one :/ */
 
 export async function generateMetadata(): Promise<Metadata> {
   const settings = await fetchSettings();
   return {
-    title:{
+    title: {
       template: `%s - ${settings[0]?.company || "Tyju Infosports"}`,
-      default: `${settings[0]?.company || "Tyju Infosports"} - L'actualité sportive`
+      default: `${settings[0]?.company || "Tyju Infosports"} - L'actualité sportive`,
     },
-    description: settings[0]?.description || "TyJu Infosports est un média en ligne dédié à l’actualité sportive, avec un focus particulier sur le sport africain, et plus précisément camerounais. Nous couvrons un large éventail de disciplines, avec pour mission de mettre en lumière les talents, les performances et les événements qui façonnent le paysage sportif du continent. Pensé et créé par deux jeunes passionnés de sport, TyJu Infosports se veut une plateforme dynamique, engagée et proche des réalités locales, pour donner la parole à celles et ceux qui font vibrer le sport au quotidien.",
-    keywords: [`${settings[0]?.company || "Tyju Infosports"}`, "sport", "football", "cameroun", "MTN Elite one", "handball", "basketball", "tyju", "infosport", "sports", "lions indomptables", "lionnes indomptables", "CHAN", "CAN"],
-    authors: [{name: `${settings[0]?.company || "Tyju Infosports"}`},],
+    description:
+      settings[0]?.description ||
+      "TyJu Infosports est un média en ligne dédié à l’actualité sportive, avec un focus particulier sur le sport africain, et plus précisément camerounais. Nous couvrons un large éventail de disciplines, avec pour mission de mettre en lumière les talents, les performances et les événements qui façonnent le paysage sportif du continent. Pensé et créé par deux jeunes passionnés de sport, TyJu Infosports se veut une plateforme dynamique, engagée et proche des réalités locales, pour donner la parole à celles et ceux qui font vibrer le sport au quotidien.",
+    keywords: [
+      `${settings[0]?.company || "Tyju Infosports"}`,
+      "sport",
+      "football",
+      "cameroun",
+      "MTN Elite one",
+      "handball",
+      "basketball",
+      "tyju",
+      "infosport",
+      "sports",
+      "lions indomptables",
+      "lionnes indomptables",
+      "CHAN",
+      "CAN",
+    ],
+    authors: [{ name: `${settings[0]?.company || "Tyju Infosports"}` }],
     publisher: `${settings[0]?.company || "Tyju Infosports"}`,
     icons: {
       icon: "/favicon.ico",
       shortcut: "/favicon.ico",
-      apple: "/apple-touch-icon.png"
+      apple: "/apple-touch-icon.png",
     },
     openGraph: {
-      title:{
+      title: {
         template: `%s - ${settings[0]?.company || "Tyju Infosports"}`,
-        default: `${settings[0]?.company || "Tyju Infosports"} - L'actualité sportive`
+        default: `${settings[0]?.company || "Tyju Infosports"} - L'actualité sportive`,
       },
-      description: settings[0]?.description || "TyJu Infosports est un média en ligne dédié à l’actualité sportive, avec un focus particulier sur le sport africain, et plus précisément camerounais. Nous couvrons un large éventail de disciplines, avec pour mission de mettre en lumière les talents, les performances et les événements qui façonnent le paysage sportif du continent. Pensé et créé par deux jeunes passionnés de sport, TyJu Infosports se veut une plateforme dynamique, engagée et proche des réalités locales, pour donner la parole à celles et ceux qui font vibrer le sport au quotidien.",
+      description:
+        settings[0]?.description ||
+        "TyJu Infosports est un média en ligne dédié à l’actualité sportive, avec un focus particulier sur le sport africain, et plus précisément camerounais. Nous couvrons un large éventail de disciplines, avec pour mission de mettre en lumière les talents, les performances et les événements qui façonnent le paysage sportif du continent. Pensé et créé par deux jeunes passionnés de sport, TyJu Infosports se veut une plateforme dynamique, engagée et proche des réalités locales, pour donner la parole à celles et ceux qui font vibrer le sport au quotidien.",
       url: process.env.NEXT_PUBLIC_URL || "https://tyjuinfosports.com",
       siteName: `${settings[0]?.company || "Tyju Infosports"}`,
       images: [
@@ -46,7 +68,7 @@ export async function generateMetadata(): Promise<Metadata> {
           alt: `${settings[0]?.company || "Tyju Infosports"}`,
         },
       ],
-    }
+    },
   };
 }
 /* export const metadata: Metadata = {
@@ -60,33 +82,38 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
   const idC = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
   return (
     <html suppressHydrationWarning>
       <head>
-        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${idC}`}></Script>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${idC}`}
+        ></Script>
         <Script id="google-analitics">
           {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-          gtag('config', '${idC}');`
-          }
+          gtag('config', '${idC}');`}
         </Script>
       </head>
       <body
-        className={cn("min-h-screen font-sans antialiased", fontMono.variable, fontSans.variable)}
+        className={cn(
+          "min-h-screen font-sans antialiased",
+          fontMono.variable,
+          fontSans.variable,
+        )}
         suppressHydrationWarning={true}
       >
         <main>
           {/* <GA /> */}
           <Providers>
             <HydrationZustand>
-              <Navbar/>
+              <Navbar />
               {children}
-              <Footbar/>
+              <Footbar />
             </HydrationZustand>
           </Providers>
           {/* <Maintenance /> */}
